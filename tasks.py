29207a6dc5638eef3af9a42831d6c16bf97f6899
Sebastian Łach <root@slach.eu>
2018-11-25 02:55:12 +0100

    Add invoke to requirements.txt

---
+++ tasks.py
@@ -0,0 +1,27 @@
+from logging import getLogger
+from invoke import task
+
+
+logger = getLogger(__name__)
+
+
+@task
+def tests(context, coverage=True, pycodestyle=True, verbose=True):
+    """Run test suit."""
+    logger.info("Running test suit")
+    context.run("nosetests -v --with-coverage")
+
+
+@task
+def docs(context, docs=False):
+    """Generate documentation."""
+    logger.info("Building documentation")
+    if docs:
+        context.run("echo sphinx-build")
+
+
+@task
+def clean(context, bytecode=False, extra=''):
+    """Clean up cache files."""
+    logger.info("Cleaning cache files")
+    context.run("rm -rf {}".format('**/*.pyc'))
